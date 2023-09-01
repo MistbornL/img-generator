a8499992fa303ee88a48f4da78b525cae6b0908d
MistbornL <lashaloria3@gmail.com>
2023-09-02 01:55:26 +0400

    fixed tailwind configuration and component form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import { Home } from "./page/Home";
-import { CreatePost } from "./page/CreatePost";
 import logo from "./assets/logo.svg";
+import Home from "./page/Home";
+import CreatePost from "./page/CreatePost";
 const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
